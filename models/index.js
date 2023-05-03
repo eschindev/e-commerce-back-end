96e5751c0e725ddda8f5c74d5bf0bc4788aa3a8e
eschindev <118208710+eschindev@users.noreply.github.com>
2023-05-03 16:52:57 -0700

    fixed realtionships in models/index.js

--- models/index.js
+++ models/index.js
@@ -5,10 +5,14 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
@@ -16,7 +20,8 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  as: 'tag_products'
+  foreignKey: 'product_id',
+  otherKey: 'tag_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
@@ -25,7 +30,8 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false
   },
-  as: 'product_tags'
+  foreignKey: 'tag_id',
+  otherKey: 'product_id'
 })
 
 module.exports = {
